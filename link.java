738026b0046007805835406ae3e295f48528ed51
Md Shahajada Hossain Hridoy <141341585+mshhridoy75@users.noreply.github.com>
2024-01-16 18:04:12 +0600

    Update and rename STQA1.java to link.java

--- link.java
+++ link.java
@@ -1,11 +1,11 @@
-package stqa1;
+package link;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class STQA1 {
+public class Link {
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\Compressed\\SQA5\\chromedriver-win64\\chromedriver.exe");
